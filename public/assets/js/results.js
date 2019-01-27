82b9909bb7b375be6e70c2bd9167ce1981292e4d
Michael High <mhigh85@gmail.com>
2019-01-26 23:22:36 -0500

    Added basic rendering logic for recipe results by category

--- public/assets/js/results.js
+++ public/assets/js/results.js
@@ -3,8 +3,16 @@
 // DOM Ready
 $(function() {
 
+  // get the URL parameters
   const urlParams = new URLSearchParams(window.location.search);
-  renderResults(urlParams.get('categoryid'));
+  
+  // get the category name and render to page
+  $.get(`/api/category/${urlParams.get('categoryId')}`)
+  .then(function(data){
+    $('.category-title').html(data.name)
+  });
+  
+  renderResults(urlParams.get('categoryId'));
 
 });
 
@@ -16,7 +24,7 @@ const tmplRecipeCard = function(recipeId, title, photoUrl) {
     <a href="#">
       <div class="card mb-3">
         <img class="card-img-top" src="${photoUrl}" alt="photo of recipe">
-        <p class="card-title pt-1">${title}</p>
+        <p class="card-title p-1">${title}</p>
       </div>
     </a>
   </div>
@@ -27,15 +35,21 @@ const renderRecipes = function(array) {
   // clear the recipes div
   $('#recipes').empty();
   
-  // foreach recipe in array, render to page using template
-  for (let i = 0; i < array.length; i++) {
-      let recipeId = array[i].recipeId;
-      let title = array[i].name;
-      let photoUrl = array[i].photoUrl;
-      $('#recipes').append(tmplRecipeCard(recipeId, title, photoUrl));
-  }
+  // if array of recipes is empty then display no result message otherwise render results
+  if(array.length === 0) {
+    $('#recipes').html(`<p class="font-italic text-muted py-3">Sorry, We don't have any recipes in this category.</p>`);
+  } else {
+    // foreach recipe in array, render to page using template
+    for (let i = 0; i < array.length; i++) {
+        let recipeId = array[i].recipeId;
+        let title = array[i].title;
+        let photoUrl = `./assets/images/${array[i].image}`;
+        $('#recipes').append(tmplRecipeCard(recipeId, title, photoUrl));
+    }
+  };
 }
 
+// Get recipes based on category and render results
 const renderResults = function(categoryId) {
   $.get(`/api/recipes?categoryId=${categoryId}`)
   .then(function(data){
