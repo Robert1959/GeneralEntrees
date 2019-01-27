e6a98bf87f6eec3cfa51300a4b06929ab75ad264
Michael High <mhigh85@gmail.com>
2019-01-26 22:22:15 -0500

    Added route for results.html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,6 +3,9 @@ module.exports = function (app) {
     app.get('/', function (req, res) {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
+    app.get('/results', function (req, res) {
+        res.sendFile(path.join(__dirname, '../public/results.html'));
+    });
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, '../public/index.html'));
     });
