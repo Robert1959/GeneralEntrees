50c5a3ee10bbb8afe18392870a0ac819db761ab8
Rob <cdrh41@gmail.com>
2019-01-30 20:33:57 -0500

    Added logic for UrlSearchParams and recipePhoto

--- public/assets/js/recipe.js
+++ public/assets/js/recipe.js
@@ -2,17 +2,19 @@
 
 // DOM Ready
 $(function() {
-    renderResults(1);
+    // get the URL parameters
+    const urlParams = new URLSearchParams(window.location.search);
+    renderResults(urlParams.get('recipeId'));
 });
    
 // Render the Recipes
 const renderRecipe = function(recipe) {
     $('.recipeTitle').html(recipe.title);
+    $('.recipePhoto').html(`<img src="${recipe.image}" alt="..." />`);
     $('.recipeServe').html(recipe.servings);
     $('.recipeInstructions').html(recipe.instructions);
     $('.recipePrep').html(recipe.prepTime);
     $('.recipeCook').html(recipe.cookTime);
-
 }
   
 // Get recipes based on category and render results
